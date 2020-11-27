41bf53dee5c8eec5d823c34ceddd4b7ae291c21d
Jason <jason27chan@gmail.com>
2020-11-27 11:51:49 -0500

    add simple cors permissions

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 const port = 3000
+
+app.use(cors()); // configure to be safer
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
