6283773aba7db6a97ced2f4976db830c3211cbe6
Jason <jason27chan@gmail.com>
2020-11-27 12:56:49 -0500

    add getTherapist endpoint to serverjs

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const querymaker = require('./querymaker');
+
 const app = express();
 const port = 3000
 
@@ -10,6 +12,15 @@ app.use(cors()); // configure to be safer
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/getTherapists', async (req, res) => {
+  try {
+    const therapist = await querymaker.getTherapists();
+    res.send(therapist);
+  } catch(e) {
+    res.send('ERROR');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
