7fae47f9b1e6a497ebde17dd0376c165840958b1
Jason <jason27chan@gmail.com>
2020-11-27 12:56:41 -0500

    add getTherapist method to querymaker

--- src/querymaker.js
+++ src/querymaker.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const Therapist = require('../models/Therapist');
+
 mongoose.connect(`${process.env.DB_HOST}/silverline`, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -8,3 +10,10 @@ db.once('open', function() {
   // we're connected!
   console.log('connected to database');
 });
+
+module.exports =  {
+    getTherapists: async () => {
+        return await Therapist.find();
+    }
+}
+
