786fad691c4b6261f8648445ab79fb306f197eb0
Jason <jason27chan@gmail.com>
2020-11-27 13:02:19 -0500

    clean up db connect text

--- src/querymaker.js
+++ src/querymaker.js
@@ -7,8 +7,7 @@ mongoose.connect(`${process.env.DB_HOST}/silverline`, {useNewUrlParser: true, us
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
-  console.log('connected to database');
+  console.log('silverline server connected to database');
 });
 
 module.exports =  {
